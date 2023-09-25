ccf6b3eb2acaa6d914b466f87c06dda25f60e6f0
CarlosAlvaZ <carlos.alvarenga017@gmail.com>
2023-09-25 15:22:34 -0600

    Added router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,11 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs"
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   
+  const { data } = api.posts.getAll.useQuery()
+
   const user = useUser()
+
 
   return (
     <>
@@ -26,9 +28,9 @@ export default function Home() {
             {!user.isSignedIn && <SignInButton />}
             {!!user.isSignedIn && <SignOutButton />}
           </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
+          <div>
+            {data?.map(post => <div key={post.id}>{post.content}</div>)}
+          </div>
         </div>
       </main>
     </>
